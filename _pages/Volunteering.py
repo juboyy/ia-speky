e7497827a744e39cf22c350b71491cdb7bf82247
João Rafael <95063152+juboyy@users.noreply.github.com>
2024-07-30 12:07:37 -0300

    ROOT commit

---
+++ _pages/Volunteering.py
@@ -0,0 +1,33 @@
+st.title("Voluntariado")
+tab1, tab2 = st.tabs(["SPYM", "NSS"])
+with tab1:
+    st.subheader("Sociedade para a Promoção da Juventude e das Massas (SPYM)")
+    st.write(
+        "A SPYM opera em níveis estadual e nacional, focando na prevenção e reabilitação de drogas para crianças e jovens, saúde (saúde reprodutiva e sexual, HIV/AIDS), habilidades de vida e educação para crianças de rua, desenvolvimento comunitário e treinamento e pesquisa."
+    )
+    st.markdown(
+    """
+    #### Voluntário na SPYM Darjeeling 2019-Presente
+    ## Papel
+    - Parte da conscientização na prevenção do abuso de drogas e álcool em diferentes instituições educacionais e sociedades em Darjeeling e arredores.
+    - Organizou uma oficina especial para trabalhadores e clientes em recuperação sobre o MS Office e os conceitos básicos de um computador.
+    - Desenvolveu e manteve o site oficial da SPYM. (2020-2021)
+    - Participou de iniciativas de saúde, com foco na saúde sexual, bem como na prevenção e conscientização do HIV/AIDS.
+    """
+)
+with tab2:
+    st.subheader("Serviço Nacional de Saúde (NSS)")
+    st.write(
+        "O Serviço Nacional de Saúde (NSS) é um programa de serviço público sob o Ministério de Assuntos da Juventude e Esportes do Governo da Índia. Foi lançado no ano do Centenário de Gandhiji, em 1969, com o objetivo de desenvolver a personalidade dos estudantes por meio do serviço comunitário."
+    )
+    st.markdown(
+    """
+    #### Presidente da Unidade NSS, Instituto de Tecnologia Manipal de Sikkim (SMIT) 2022-2023
+    ## Papel
+    - Organizar atividades mensais e campanhas de conscientização sobre várias questões sociais.
+    - Coordenar acampamentos de saúde mensais e promover conscientização básica de higiene em diferentes sociedades em Sikkim.
+    - Estabelecer estratégias operacionais dentro da equipe para aumentar a eficácia do clube.
+    - Iniciar projetos ecologicamente corretos, como plantio de árvores e programas de conscientização sobre gestão de resíduos.
+    - Coordenar com ONGs locais e órgãos governamentais para projetos colaborativos de serviço comunitário.
+    """
+)
