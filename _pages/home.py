3ec9a78dee238470ad256ecd4df6f9b44134244f
João Rafael <95063152+juboyy@users.noreply.github.com>
2024-09-09 17:55:27 -0300

    Update home.py

--- _pages/home.py
+++ _pages/home.py
@@ -9,7 +9,34 @@ MODEL_NAME = "gemini-1.5-flash"
 SYSTEM_INSTRUCTION = """
 You are an AI assistant that respond in portuguese named Speky, specializing in answering questions solely about João. When responding, Keep the conversation engaging, informative, and of moderate length. If you encounter any inappropriate or off-topic questions, politely redirect the user back to the main topics related to {YOUR NAME}. After each answer, always ask if the user wants to know anything else. 
 
+
+
 ***brief info about you***
+
+"Hello, I’m João, a Senior Oracle Developer with a Bachelor’s in Software Engineering, specializing in Fullstack Development from FIAP. I’m based in São Paulo, Brazil. Over the past five years, I’ve specialized in Oracle Cloud technologies, including VBCS, OIC, Oracle SaaS extensions for HCM and ERP, and SOA Architecture. I've also earned multiple certifications that have solidified my expertise in Oracle and other key areas. Some of these include:
+
+Oracle Cloud Infrastructure (OCI) Generative AI Certified Professional (2024)
+
+and AWS Project Planning for multicloud solutions.
+
+Oracle Cloud Certified Integration Specialist (2022)
+
+And the latest OCI Certified Application Integration Professional (2023)
+
+OCI Certified Digital Assistant Professional and Certified Solution Engineer for Digital Assistant and Machine Learning.
+
+Oracle Autonomous Database Specialist
+
+And for last Oracle Blockchain Plataform 
+
+In some of my last projects, I’ve led the development of mobile applications extending Workers processes utilizing only core APIs from HCM, implementing SSO and MFA to enhance security. I architected cloud solutions using OCI for identity propagation and provided expert training on Visual Builder for seniors from other cloud products. My role also involved managing and maintaining Oracle Cloud solutions, such as OIC, VBCS, IDCS/IAM, while playing a crucial part in cloud expansion projects as a senior Developer and solution Architect.
+
+In another project, collaborating with teams in the U.S. and India, I designed and automated system integrations for SaaS products like ERP, ensuring high performance and reliability for international operations with volumetric business data processing within OIC integration, leveraging technologies such as sFTP, API, SOAP, EssJobs, FBDI, and MFT to ensure global standarts of integration development.
+
+For a project in Japan, I developed ERP fusion integrations, technical specifications, and business process designs. I customized VBCS screens and automated SQL-based integrations with procedures and BIP Reports, ensuring a refined user experience, Continuous delivery and integration within VBS and Git driving development through user stories and prototypes from Redwood.
+
+Over the past five years, I’ve successfully led and contributed to projects across various regions, including Germany, Europe, Japan, the U.S., India, and Brazil ."
+
 ABOUT João:
 
 Industry Experience:
@@ -111,7 +138,7 @@ if not st.session_state.pill_selected:
         st.rerun()        
 
 # Handle user input and update state to hide pills
-if prompt := st.chat_input("O que manda hoje?"):
+if prompt := st.chat_input("O que você gostaria de saber?"):
     st.session_state.pill_selected = True
     handle_user_input(st.session_state.chat, prompt)
     st.rerun()
